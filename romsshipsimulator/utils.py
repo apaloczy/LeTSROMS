722afd745cadc8b5487899dbbf0fddd518ae47f0
André Palóczy <paloczy@gmail.com>
2017-05-27 15:51:18 -0700

    Add utils.

---
+++ romsshipsimulator/utils.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+#
+# Description: Utilities for working with
+#              the RomsShipSimulator class.
+#
+# Author:      André Palóczy
+# E-mail:      paloczy@gmail.com
+# Date:        May/2017
+
+import numpy as np
+
+__all__ = [mk_shiptrack]
+
+def mk_shiptrack():
+    """
+    Creates a ship track (longitude, latitude, time)
+    to serve as input to a RomsShipSimulator instance.
+    """
+    return 1
