f8af0041b1d2d15ffcd05ace2cfb33e1535cc144
André Palóczy <paloczy@gmail.com>
2017-05-29 23:25:22 -0700

    Constructing utils.mk_shiptrack().

--- romsshipsimulator/utils.py
+++ romsshipsimulator/utils.py
@@ -12,7 +12,7 @@ __all__ = ['mk_shiptrack', 'ShipTrackError']
 import numpy as np
 from pygeodesy.sphericalNvector import LatLon
 
-def mk_shiptrack(waypts, sampfreq, shipspd=4, evenspacing=False, closedtrk=False, repeat=1, verbose==False):
+def mk_shiptrack(waypts, sampfreq, shipspd=4, evenspacing=False, closedtrk=False, nrepeat=1, verbose=True):
     """
     Creates a ship track (longitude, latitude, time)
     to serve as input to a RomsShipSimulator instance.
@@ -24,11 +24,11 @@ def mk_shiptrack(waypts, sampfreq, shipspd=4, evenspacing=False, closedtrk=False
     sampfreq:  Ship sampling frequency (measurements/h).
     shipspd:   Underway speed [kn], 1 kn = 1 nm/h = 1.852 km/h ~ 0.5 m/s.
     closedtrk: Whether to connect the last waypoint to the first.
-    repeat:    Number of occupations of the track.
+    nrepeat:   Number of realizations of the track.
                * If 'closedtrk' is 'False', moves the ship back and
-               forth along the waypoints, 'repeat' times.
+               forth along the waypoints, 'nrepeat' times.
                * If 'closedtrk' is 'True', moves the ship along the
-               polygon formed by the waypoints, 'repeat' times.
+               polygon formed by the waypoints, 'nrepeat' times.
 
     OUTPUT
     ------
@@ -39,10 +39,11 @@ def mk_shiptrack(waypts, sampfreq, shipspd=4, evenspacing=False, closedtrk=False
     TODO
     ----
     Implement 'evenspacing' option.
-    Implement verbose messages (track lengths, numer of points).
+    Implement SHIP TIME CALCULATION and return it with lon, lats.
     """
     lons, lats = map(np.array, (waypts[0], waypts[1]))
     assert lons.size==lats.size, "Different number of longitudes and latitudes."
+
     if closedtrk and not np.logical_and(lons[0]==lons[-1], lats[0]==lats[-1]):
         raise ShipTrackError("First and last waypoints must be identical for closed ship tracks.")
 
@@ -51,21 +52,25 @@ def mk_shiptrack(waypts, sampfreq, shipspd=4, evenspacing=False, closedtrk=False
     sampfreq = sampfreq/3600 # convert sampling frequency to measurements/s.
     dshp = shipspd/sampfreq # Spatial separation between measurements [m].
     trkpts = []
-    for noc in range(noccupations):
+    for nrep in range(nrepeat):
         if verbose:
-            print("")
-        for n in range(nwaypts-1):
+            print("Realization %d/%d\n"%(nrep+1, nrepeat))
+        nsegs = nwaypts - 1
+        for n in range(nsegs):
             wptA = LatLon(lats[n], lons[n])
             wptB = LatLon(lats[n+1], lons[n+1])
             dAB = wptA.distanceTo(wptB) # length of current segment [m].
             dfrac = dshp/dAB # Separation between sample points as fraction of segment.
             nn = int(1/dfrac) - 1 # Number of points that fit in this segment (excluding waypoints A and B).
-            print(round(dAB*1e-3, 1), round(dfrac, 3), nn)
+            # print(round(dAB*1e-3, 1), round(dfrac, 3), nn)
             if nn==-1:
                 raise ShipTrackError('Segment from %s to %s is not long enough to accomodate any ship sampling points.'%(wptA.toStr(), wptB.toStr()))
+            if verbose:
+                print("Segment %d/%d:  %s --> %s (%.3f km),"%(n+1, nsegs, wptA.toStr(), wptB.toStr(), dAB*1e-3))
             trkpts.append([wptA.intermediateTo(wptB, dfrac*ni) for ni in range(nn)])
+        print("\n")
 
-    return trkpts
+    return np.array(trkpts)
     # return xship, yship, tship
 
 class ShipTrackError(Exception):
