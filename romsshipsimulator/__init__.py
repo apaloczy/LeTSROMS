d9d1f5935456bfda32689afad7b692cedc707ccd
André Palóczy <paloczy@gmail.com>
2017-05-29 20:58:56 -0700

    Clean up.

---
+++ romsshipsimulator/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from .romsshipsimulator import (RomsShipSimulator)
+from .romsshipsimulator import utils
+
+__all__ = [
+    'RomsShipSimulator',
+    'utils'
+    ]
+
+__version__ = '0.1beta'
