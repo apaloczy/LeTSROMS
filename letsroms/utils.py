e6418c0b06d05deb8be0767722d2dbc659d2b0c9
André Palóczy <paloczy@gmail.com>
2017-07-07 00:40:18 -0700

    Convert 'mk_shiptrack()' to 'ShipTrack' class.

--- letsroms/utils.py
+++ letsroms/utils.py
@@ -1,23 +1,24 @@
 # Description: Utilities for working with
-#              the RomsShip class.
+#              classes in LeTSROMS.
 # Author/date: André Palóczy, May/2017.
 # E-mail:      paloczy@gmail.com
 
-__all__ = ['mk_shiptrack',
+__all__ = ['ShipTrack',
            'ShipTrackError']
 
 import numpy as np
 from datetime import datetime, timedelta
 from pygeodesy.sphericalNvector import LatLon
+from xarray import DataArray, Variable
 
 
-def mk_shiptrack(waypts, tstart, sampfreq, shipspd=4, evenspacing=False, closedtrk=False, nrepeat=1, verbose=True):
+class ShipTrack(object):
     """
     USAGE
     -----
-    mk_shiptrack(waypts, tstart, sampfreq, shipspd=4, evenspacing=False, closedtrk=False, nrepeat=1, verbose=True)
+    ShipTrack(waypts, tstart, sampfreq=12, shipspd=4, evenspacing=False, closedtrk=False, nrepeat=1, verbose=True)
 
-    Creates a ship track (longitude, latitude, time)
+    Creates a ship track object (longitude, latitude, time)
     to serve as input to a RomsShip instance.
 
     INPUT
@@ -50,54 +51,120 @@ def mk_shiptrack(waypts, tstart, sampfreq, shipspd=4, evenspacing=False, closedt
 
     TODO
     ----
-    Convert to a class 'ShipTrack'.
     Implement 'evenspacing' option.
     """
-    lons, lats = map(np.array, (waypts[0], waypts[1]))
+    def __init__(self, waypts, tstart, sampfreq=12, shipspd=4, evenspacing=False, closedtrk=False, nrepeat=1, verbose=True):
+        lons, lats = map(np.array, (waypts[0], waypts[1]))
+        self.lons, self.lats = lons, lats
 
-    if closedtrk and not np.logical_and(lons[0]==lons[-1], lats[0]==lats[-1]):
-        raise ShipTrackError("First and last waypoints must be identical for closed ship tracks.")
+        if closedtrk and not np.logical_and(lons[0]==lons[-1], lats[0]==lats[-1]):
+            raise ShipTrackError("First and last waypoints must be identical for closed ship tracks.")
 
-    nwaypts = lons.size
-    shipspd = shipspd*1852/3600 # kn to m/s.
-    sampfreq = sampfreq/3600    # measurements/h to measurements/s.
-    sampdt = 1/sampfreq         # Time between adjacent measurements [s].
-    dshp = shipspd/sampfreq     # Spatial separation between adjacent measurements [m].
-    nsegs = nwaypts - 1
-    trktimesi = tstart
-    trktimes = []
-    trkpts = []
-    for nrep in range(nrepeat):
-        if verbose:
-            print("Realization %d/%d\n"%(nrep+1, nrepeat))
-        for n in range(nsegs):
-            wptA = LatLon(lats[n], lons[n])
-            wptB = LatLon(lats[n+1], lons[n+1])
-            dAB = wptA.distanceTo(wptB) # length of current segment [m].
-            tAB = dAB/shipspd # Occupation time of current segment [s].
-            dfrac = dshp/dAB # Separation between sample points
-                             # as a fraction of the segment.
-            nn = int(1/dfrac) - 1 # Number of points that fit in this segment (excluding waypoints A and B).
-            if nn==-1:
-                raise ShipTrackError('Segment from %s to %s is not long enough to accomodate any ship sampling points.'%(wptA.toStr(), wptB.toStr()))
+        self.shipspd = Variable(dims='', data=np.array([shipspd]),
+                                attrs=dict(units='kn, 1 kn = 1 nm/h = 1.852 km/h'))
+        self.sampfreq = Variable(dims='', data=np.array([sampfreq]),
+                                 attrs=dict(units='measurements/h',
+                                 long_name='Sampling frequency'))
+        self.sampdt = Variable(dims='', data=np.array([60/sampfreq]),
+                               attrs=dict(units='minutes',
+                               long_name='Time between adjacent measurements'))
+        self.tstart = tstart
+        self.is_evenspacing = evenspacing
+        self.is_closedtrk = closedtrk
+        self.nrepeat = nrepeat
+        self.nwaypts = lons.size
+        self.nsegs = self.nwaypts - 1
+        shipspd = shipspd*1852/3600 # kn to m/s.
+        sampfreq = sampfreq/3600    # measurements/h to measurements/s.
+        sampdt = 1/sampfreq         # Time between adjacent measurements [s].
+        dshp = shipspd/sampfreq     # Spatial separation between adjacent measurements [m].
+        self.dshp = Variable(dims='', data=np.array([dshp]),
+                             attrs=dict(units='m',
+                             long_name='Separation between adjacent measurements'))
+        trktimesi = tstart
+        trktimes = []
+        trkpts = []
+        seg_lenghts = []
+        seg_times = []
+        seg_npoints = []
+        occupation_number = np.array([])
+        for nrep in range(nrepeat):
+            nrepp = nrep + 1
             if verbose:
-                print("Segment %d/%d:  %s --> %s (%.3f km | %.2f h)"%(n+1, nsegs, wptA.toStr(), wptB.toStr(), dAB*1e-3, tAB/3600))
-            trkptsi = [wptA.intermediateTo(wptB, dfrac*ni) for ni in range(nn)]
-            trktimesi = [trktimesi + timedelta(sampdt*ni/86400) for ni in range(nn)]
-            # Fix actual start time of next segment by accounting for the
-            # time to cover the distance between last sample point and wptB.
-            ttransit = trkptsi[-1].distanceTo(wptB)/shipspd
-            endsegtcorr = trktimesi[-1] + timedelta(ttransit/86400)
-            trkptsi.append(wptB)
-            trktimesi.append(endsegtcorr)
-            trkpts.append(trkptsi)
-            trktimes.append(trktimesi)
-            trktimesi = endsegtcorr # Keep most recent time for next line.
-            trkptsi = wptB # Keep most recent time for next line.
-        if verbose:
-            print("\n")
+                print("Realization %d/%d\n"%(nrepp, nrepeat))
+            for n in range(self.nsegs):
+                wptA = LatLon(lats[n], lons[n])
+                wptB = LatLon(lats[n+1], lons[n+1])
+                dAB = wptA.distanceTo(wptB) # length of current segment [m].
+                tAB = dAB/shipspd # Occupation time of current segment [s].
+                dfrac = dshp/dAB  # Separation between sample points
+                                  # as a fraction of the segment.
+                nn = int(1/dfrac) - 1 # Number of points that fit in this
+                                      # segment (excluding waypoints A and B).
+                if nn==-1:
+                    raise ShipTrackError('Segment from %s to %s is not long enough to accomodate any ship sampling points.'%(wptA.toStr(), wptB.toStr()))
+                if verbose:
+                    print("Segment %d/%d:  %s --> %s (%.3f km | %.2f h)"%(n+1, self.nsegs, wptA.toStr(), wptB.toStr(), dAB*1e-3, tAB/3600))
+                trkptsi = [wptA.intermediateTo(wptB, dfrac*ni) for ni in range(nn)]
+                trktimesi = [trktimesi + timedelta(sampdt*ni/86400) for ni in range(nn)]
+                # Fix actual start time of next segment by accounting for the
+                # time to cover the distance between last sample point and wptB.
+                ttransit = trkptsi[-1].distanceTo(wptB)/shipspd
+                endsegtcorr = trktimesi[-1] + timedelta(ttransit/86400)
+                trkptsi.append(wptB)
+                trktimesi.append(endsegtcorr)
+                nptsseg = nn + 1
+                seg_npoints.append(nptsseg)
+                trkpts.append(trkptsi)
+                trktimes.append(trktimesi)
+                trktimesi = endsegtcorr # Keep most recent time for next line.
+                trkptsi = wptB          # Keep last point for next line.
+                seg_lenghts.append(dAB*1e-3)
+                seg_times.append(tAB/3600)
+                # Keep number of the current occupation as a coordinate.
+                occupation_number = np.append(occupation_number,
+                                              np.array([nrepp]*nptsseg))
+            if verbose:
+                print("\n")
 
-    return np.array(trkpts), np.array(trktimes)
+        # Store times and coordinates of the points along the track.
+        attrspts = dict(long_name='Lon/lat coordinates of points sampled along the ship track')
+        attrstimes = dict(long_name='Times of points sampled along the ship track')
+        attrsocc = dict(long_name='')
+        assert len(trkpts)==len(trktimes)
+        dim = 'point number'
+
+        self.trkpts = Variable(data=trkpts, dims=dim, attrs=attrspts)
+        self.trktimes = Variable(data=trktimes, dims=dim, attrs=attrstimes)
+        self.occupation_number = Variable(data=occupation_number, dims=dim,
+                                          attrs=attrsocc)
+
+        segment_number = np.arange(self.nsegs*self.nrepeat) + 1
+        seg_coords = {'segment number':segment_number}
+        seg_dims = 'segment number'
+        self.seg_lenghts = DataArray(seg_lenghts, coords=seg_coords,
+                                     dims=seg_dims, name='Length of each track segment')
+        self.seg_times = DataArray(seg_times, coords=seg_coords, dims=seg_dims,
+                                   name='Duration of each track segment')
+        self.seg_npoints = DataArray(seg_npoints, coords=seg_coords,
+                                     dims=seg_dims,
+                                     name='Number of points sampled on each track segment')
+
+    def xtrk_flux(self, varname, transp=False, synop=False, segwise_synop=False, **kw):
+        """
+        Calculate the cross-track mean (u_mean*Q_mean) eddy flux (u'*Q') for
+        a variable "Q" sampled from ROMS output. All kwargs are
+        passed to RomsShip.ship_sample().
+
+        The mean is defined as the average of Q over each track line, such that
+        u = u_mean + u' and Q = Q_mean + Q' are the total cross-track velocity
+        and value of Q, respectively.
+
+        if 'transp' is True (defaults to False), then the mean and eddy
+        transports (i.e., the mean and eddy fluxes integrated in the vertical
+        and in the along-track direction) are returned instead of the fluxes.
+        """
+        return 1
 
 
 class ShipTrackError(Exception):
