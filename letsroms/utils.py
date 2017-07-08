b43887f16b22b80365f61a761d7e1fb9ee50b63c
André Palóczy <paloczy@gmail.com>
2017-07-08 02:19:40 -0700

    Add helper functions, building 'crosstrk_flux()'.

--- letsroms/utils.py
+++ letsroms/utils.py
@@ -4,10 +4,12 @@
 # E-mail:      paloczy@gmail.com
 
 __all__ = ['crosstrk_flux',
-           'compass2trig']
+           'compass2trig',
+           'strip']
 
 import numpy as np
 from datetime import datetime, timedelta
+from pygeodesy.utils import wrap180
 from pygeodesy.sphericalNvector import LatLon
 from xarray import DataArray, Variable
 from ap_tools.utils import rot_vec
@@ -25,31 +27,57 @@ def crosstrk_flux(Romsship, varname, transp=False, synop=False, segwise_synop=Fa
 
     if 'transp' is True (defaults to False), then the mean and eddy
     transports (i.e., the mean and eddy fluxes integrated in the vertical
-    and in the along-track direction) are returned instead of the fluxes.
+    and in the along-track direction) for each segment of the track are
+    returned instead of the fluxes. Returns flux(segment index) instead
+    of flux(x, z).
     """
-    # Get synoptic sample.
-    usynop = Romsship.ship_sample('u', synop=True, \
-                                  segwise_synop=segwise_synop, **kw)
-    vsynop = Romsship.ship_sample('v', synop=True, \
-                                  segwise_synop=segwise_synop, **kw)
-    synopflx = Romsship.ship_sample(varname, synop=True, \
-                                    segwise_synop=segwise_synop, **kw)
-    # Get quasi-synoptic sample.
-    uship = Romsship.ship_sample('u', synop=False, **kw)
-    vship = Romsship.ship_sample('v', synop=False, **kw)
-    shipflx = Romsship.ship_sample(varname, synop=False, **kw)
+    assert isinstance(varname, str), "'varname' must be a string"
+    # assert isinstance(Romsship, RomsShip), "Input must be a letsroms.RomsShip instance" # FIXME.
 
+    uship = Romsship.ship_sample('u', synop=synop, segwise_synop=segwise_synop, **kw)
+    vship = Romsship.ship_sample('v', synop=synop, segwise_synop=segwise_synop, **kw)
+    shipvar = Romsship.ship_sample(varname, synop=synop, segwise_synop=segwise_synop, **kw)
+
+    uship = strip(uship)
+    vship = strip(vship)
+    shipvar = strip(shipvar)
     # Rotate velocities to along-/across-track coordinates.
-    ang_trk = Romsship.Shiptrack.
-    usynop, vsynop = rot_vec(usynop, vsynop, angle=ang_trk, degrees=False)
-    uship, vship = rot_vec(uship, vship, angle=ang_trk, degrees=False)
+    # Cross-track velocity (u) is positive to the RIGHT of the track.
+    # Rotate back to east-west coordinates and then to track coordinates.
+    ang_trk = Romsship.angship
+    ang_grd = Romsship.angle/Romsship._deg2rad # [degrees].
+    xsrad = strip(Romsship.xship)*Romsship._deg2rad
+    ysrad = strip(Romsship.yship)*Romsship._deg2rad
+    ang_grd =  Romsship._interpxy(ang_grd, xsrad, ysrad, shipvar._interpm, 'rho')
+    ang_tot = ang_trk - ang_grd # [degrees].
+    _, uship = rot_vec(uship, vship, angle=ang_tot, degrees=True)
+    uship = -uship
 
-    return 1
+    # Calculate cross-track fluxes.
+    dA = shipvar.dA
+    for n in range(self.nsegs):
+        n+=1
+        fseg=self.segment_index==n
+        uQcov_a = uship[fseg]*shipvar[fseg]*dA[fseg]
+        uQcov_avg = np.append(uQcov_avg, da*np.sum(uQcov_a)/da.sum())
+    uship_bar, Qbar = uship*dA,
+
+    return uQbar, uQeddy
 
 
-    def compass2trig(ang_compass):
-        aux = ang_compass - 90         # Move origin to Eastward direction.
-        aux = wrap180(aux)
-        ang_trig = -aux # Increases couterclockwise (trig convention).
+def compass2trig(ang_compass, input_in_degrees=True):
+    if not input_in_degrees:
+        ang_compass = ang_compass*180/np.pi # Radians to degrees.
+    aux = wrap180(ang_compass - 90)         # Move origin to Eastward direction.
+    ang_trig = -aux                         # Increases couterclockwise (trig convention).
 
-        return ang_trig
+    return ang_trig
+
+
+def strip(obj):
+    if obj.fromDataArray:
+        obj = obj.vship_DataArray.data
+    else:
+        obj = obj.vship
+
+    return obj
